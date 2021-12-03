bade75b64025e6c83d28ccb1f9eb7abb4ca92dab
Le Michel <95184938+Ptitet@users.noreply.github.com>
2021-12-03 18:34:25 +0100

    Premier commit : ajout des fichiers .gitignore, package.json, package-lock.json et passwd.js

---
+++ passwd.js
@@ -0,0 +1,90 @@
+const {SHA512: sha512, MD5: md5, SHA256: sha256} = require('crypto-js');
+
+async function crack(password, method) {
+
+    const start = Date.now();
+
+    const dico = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+
+    console.log(`Cracking ${password} (using ${method})...`);
+
+    switch(method) {
+        case 'md5': {
+            let guess = 'a';
+            let eguess = md5(guess).toString();
+            let test = 1;
+
+            while(eguess !== password) {
+                let last = guess[guess.length - 1];
+                if (last === '9') {
+                    let arr = guess.split('');
+                    for (i in arr) {
+                        if (arr[arr.length - i - 1] === '9') {
+                            arr.splice(arr.length - i - 1, 1, 'a');
+                            if (!arr[arr.length - i - 2]) arr.unshift('a');
+                            else if (!(arr[arr.length - i - 2] === '9')) {
+                                arr.splice(arr.length - i - 2, 1, dico[dico.indexOf(arr[arr.length - i - 2]) + 1]);
+                                break;
+                            }
+                        }
+                    }
+                    guess = arr.join('');
+                } else {
+                    guess = `${guess.slice(0, -1)}${dico[dico.indexOf(last) + 1]}`;
+                }
+                eguess = md5(guess).toString();
+                if (test % 1000000 === 0) console.log(`Current test : ${guess} (${eguess})\nTest n°${test}`);
+                test ++;
+            }
+
+            const time = Date.now() - start;
+
+            console.log(`Your password is : ${guess}\nTime taken : ${time} ms\nTotal test : ${test}`);
+
+            process.exit(0);
+        }
+
+        case 'sha256': {
+            let guess = 'a';
+            let eguess = sha256(guess).toString();
+            let test = 1;
+
+            while(eguess !== password) {
+                let last = guess[guess.length - 1];
+                if (last === '9') {
+                    let arr = guess.split('');
+                    for (i in arr) {
+                        if (arr[arr.length - i - 1] === '9') {
+                            arr.splice(arr.length - i - 1, 1, 'a');
+                            if (!arr[arr.length - i - 2]) arr.unshift('a');
+                            else if (!(arr[arr.length - i - 2] === '9')) {
+                                arr.splice(arr.length - i - 2, 1, dico[dico.indexOf(arr[arr.length - i - 2]) + 1]);
+                                break;
+                            }
+                        }
+                    }
+                    guess = arr.join('');
+                } else {
+                    guess = `${guess.slice(0, -1)}${dico[dico.indexOf(last) + 1]}`;
+                }
+                eguess = sha256(guess).toString();
+                if (test % 1000000 === 0) console.log(`Current test : ${guess} (${eguess})\nTest n°${test}`);
+                test ++;
+            }
+
+            const time = Date.now() - start;
+
+            console.log(`Your password is : ${guess}\nTime taken : ${time} ms\nTotal test : ${test}`);
+
+            process.exit(0);
+        }
+    }
+}
+
+crack(md5('azerty').toString(), 'md5');
+
+//ab = 187ef4436122d1cc2f40dc2b92f0eba0
+//hell0 = 73b43f17232b391b9123adf40c1b65dd
+//L0Zs = b01279944c7300116289e08b61be2149
+//b9Z7 = 3fa0566ee34328a372276f8676cb7570
+//aQ9 = aa771b7ed9bd1500fa8e24de7b05114a
